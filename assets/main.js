6c0ea5b6556e178d4c1643294dab92df7686bb1c
porquezin <renatomagno75@gmail.com>
2022-05-04 16:05:18 -0300

    .

--- assets/main.js
+++ assets/main.js
@@ -1,35 +1,73 @@
 var table = []
 var linha, coluna
+var b, primeira_jogada
 
-function gerar_tabela(l, c) { //função feita somente para gerar o tabuleiro!
+
+function difculdade(dfc) {
+    primeira_jogada = true
+    switch (dfc) {
+        case 1:
+            gerar_tabela(8, 10, 10)
+            break;
+        case 2:
+            gerar_tabela(14, 18, 40)
+            break;
+        case 3:
+            gerar_tabela(20, 24, 99)
+            break;
+    }
+}
+
+function gerar_tabela(l, c, bb) { //função feita somente para gerar o tabuleiro!
     table = []
-    linha = l - 1
-    coluna = c - 1
+    linha = l
+    coluna = c
+    b = bb
+
     document.querySelector("#tabe").innerHTML = ""
-    for (i = 0; i < l; i++) {
+    for (i = 0; i < (c+1); i++) {
         table[i] = []
-        for (j = 0; j < c; j++) {
+        for (j = 0; j < (l+1); j++) {
             table[i][j] = 0
-            document.querySelector("#tabe").innerHTML += "<input type='button' id=" + i + "|" + j + " value='0'>"
+            document.querySelector("#tabe").innerHTML += "<input onclick='jogar(" + j + "," + i + ")' type='button' id=" + j + "|" + i + " value='0'>"
         }
         document.querySelector("#tabe").innerHTML += "<br>"
     }
-    console.table(table)
 }
-function gerar_bombas(b) {
+
+function gerar_bombas() {
     var bomba = 0
-    console.table(table)
     while (bomba < b) {
-        x = Math.floor(Math.random() * (linha - 0 + 1)) + 0
-        y = Math.floor(Math.random() * (coluna - 0 + 1)) + 0
-        if (table[x][y] == 0) {
-            table[x][y] = 1
-            document.getElementById(x+"|"+y).value=1
+        x = Math.floor(Math.random() * (linha))
+        y = Math.floor(Math.random() * (coluna))
+        if (table[y][x] == 0) {
+            table[y][x] = 1
+            document.getElementById(x + "|" + y).value = 1
             bomba++
-        }else{
-            document.getElementById(x+"|"+y).value=1
         }
     }
-    console.log(bomba)
+   /* for(i=0;i<coluna;i++){
+        for(j=0;j<linha;j++){
+            if(table[i][j]!=0){
+                if(table[i+1][j]!=1 ||table[i][j+1]!=1){
+                    table[i+1][j]+='a'
+                    table[i][j+1]+='a'
+                    table[i+1][j+1]+='a'
+                    table[i-1][j]+='a'
+                    table[i][j-1]+='a'
+                    table[i-1][j-1]+='a'
+                    table[i+1][j-1]+='a'
+                    table[i-1][j+1]+='a'
+                }
+            }
+        }
+    }*/
     console.table(table)
+}
+
+function jogar(li, co) {
+    if (primeira_jogada) {
+        gerar_bombas()
+        primeira_jogada = false
+    }
 }
